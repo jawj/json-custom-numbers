aea891d0107fc23020f1179b64aae98d48015aa8
George MacKerron <george@mackerron.co.uk>
2023-05-26 09:39:04 +0100

    Tweaks

--- src/parseStrict.mjs
+++ src/parseStrict.mjs
@@ -1,5 +1,5 @@
 /*
-  2023-05-25 / George MacKerron (mackerron.com)
+  2023-05-26 / George MacKerron (mackerron.com)
   Based on https://github.com/douglascrockford/JSON-js/blob/03157639c7a7cddd2e9f032537f346f1a87c0f6d/json_parse.js
   Public Domain
   NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
@@ -20,16 +20,10 @@ const stringChunkRegExp = /[^"\\\n\t\u0000-\u001f]*/y;
 const wordRegExp = /-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][-+]?[0-9]+)?|true|false|null/y;
 
 // this array is indexed by the char code of an escape character 
-// e.g. \n -> 'n'.charCodeAt(0) === 110, so escapes[110] === '\n'
+// e.g. \n -> 'n'.charCodeAt() === 110, so escapes[110] === '\n'
 const escapes = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "\"", "", "", "", "", "", "", "", "", "", "", "", "", "/", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "\\", "", "", "", "", "", "\b", "", "", "", "\f", "", "", "", "", "", "", "", "\n", "", "", "", "\r", "", "\t"];
 
-// these arrays are indexed by the char code of a unicode escape hex digit;
-// they store the relevant value for each of the 4 digit positions,
-// but with 1 added so we can distinguish '0' from an invalid hex digit
-// e.g. \u1234
-// -> the '1' in position 1 is worth 1 << 12 = 4096 
-// -> '1'.charCodeAt(0) === 49
-// -> hexLookup1[49] === 4096 + 1 = 4097
+// these are indexed by the char code of a hex digit used for \uXXXX escapes
 const hexLookup1 = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4097, 8193, 12289, 16385, 20481, 24577, 28673, 32769, 36865, 0, 0, 0, 0, 0, 0, 0, 40961, 45057, 49153, 53249, 57345, 61441, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40961, 45057, 49153, 53249, 57345, 61441]);
 const hexLookup2 = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 257, 513, 769, 1025, 1281, 1537, 1793, 2049, 2305, 0, 0, 0, 0, 0, 0, 0, 2561, 2817, 3073, 3329, 3585, 3841, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2561, 2817, 3073, 3329, 3585, 3841]);
 const hexLookup3 = new Uint16Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 17, 33, 49, 65, 81, 97, 113, 129, 145, 0, 0, 0, 0, 0, 0, 0, 161, 177, 193, 209, 225, 241, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 161, 177, 193, 209, 225, 241]);
@@ -114,14 +108,14 @@ function array() {
   // the '< 33' helps performance by short-circuiting the four other conditions in most cases
   do { ch = text.charCodeAt(at++) } while (ch < 33 && (ch === 32 || ch === 10 || ch === 13 || ch === 9));
   if (ch === 93 /* ] */) {
-    ch = text.charCodeAt(at++)
+    ch = text.charCodeAt(at++);
     return arr;  // empty array
   }
   while (ch >= 0) {  // i.e. !isNaN(ch)
     arr[i++] = value();
     while (ch < 33 && (ch === 32 || ch === 10 || ch === 13 || ch === 9)) ch = text.charCodeAt(at++);
     if (ch === 93 /* ] */) {
-      ch = text.charCodeAt(at++)
+      ch = text.charCodeAt(at++);
       return arr;
     }
     if (ch !== 44 /* , */) error("Expected ',' but got " + chDesc() + " after array element");
