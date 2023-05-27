c9f71d5ff55186fcc593210ff7bae47a50e7897c
George MacKerron <george@mackerron.co.uk>
2023-05-27 13:30:38 +0100

    Updates to build in package.json

--- test/test.mjs
+++ test/test.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import col from 'colors/safe.js';
-import { parse } from '../src/parseStrict.mjs';
+import { parse } from '../src/parse.mjs';
 import { parse as crockford } from './test_comparison/crockford.mjs';
 import { parse as parseBigInt } from 'json-bigint';
 
