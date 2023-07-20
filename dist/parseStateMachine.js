6f91ef8ad3b14892401fc18bc9a24de6747b623f
George MacKerron <george@mackerron.co.uk>
2023-07-20 21:47:34 +0100

    Build

--- dist/parseStateMachine.js
+++ dist/parseStateMachine.js
@@ -1,54 +1,55 @@
-"use strict";var k=Object.defineProperty;var N=Object.getOwnPropertyDescriptor;var j=Object.getOwnPropertyNames;var q=Object.prototype.hasOwnProperty;var L=(o,i)=>{for(var d in i)k(o,d,{get:i[d],enumerable:!0})},R=(o,i,d,l)=>{if(i&&
-typeof i=="object"||typeof i=="function")for(let r of j(i))!q.call(o,r)&&r!==d&&
-k(o,r,{get:()=>i[r],enumerable:!(l=N(i,r))||l.enumerable});return o};var X=o=>R(k({},"__esModule",{value:!0}),o);var z={};L(z,{JSONParseError:()=>v,parse:()=>P});module.exports=X(z);class v extends Error{}
-const x=["JSON value","end of input","first key in object","key in object","colo\
-n","value in object","comma or closing brace for object","first value in array",
-"value in array","comma or closing bracket for array"],C=/[^"\\\u0000-\u001f]*/y,
-y=/-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][-+]?[0-9]+)?|true|false|null/y,n="",D=[n,n,
-n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,'"',n,n,n,n,n,n,
-n,n,n,n,n,n,"/",n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,
-n,n,n,n,n,n,n,n,n,n,n,n,"\\",n,n,n,n,n,"\b",n,n,n,"\f",n,n,n,n,n,n,n,`
-`,n,n,n,"\r",n,"	"],p="",U=[],e=65536,J=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,
+"use strict";var $=Object.defineProperty;var I=Object.getOwnPropertyDescriptor;var b=Object.getOwnPropertyNames;var m=Object.prototype.hasOwnProperty;var E=(c,r)=>{for(var u in r)$(c,u,{get:r[u],enumerable:!0})},j=(c,r,u,p)=>{if(r&&
+typeof r=="object"||typeof r=="function")for(let i of b(r))!m.call(c,i)&&i!==u&&
+$(c,i,{get:()=>r[i],enumerable:!(p=I(r,i))||p.enumerable});return c};var L=c=>j($({},"__esModule",{value:!0}),c);var q={};E(q,{JSONParseError:()=>v,parse:()=>D});module.exports=L(q);class v extends Error{}
+const f=["JSON value","end of input","first key in object","key in object","':'",
+"value in object","',' or '}' in object","first value in array","value in array",
+"',' or ']' in array"],C=/[^"\\\u0000-\u001f]*/y,y=/-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][-+]?[0-9]+)?|true|false|null/y,
+n="",R=[n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,'"',
+n,n,n,n,n,n,n,n,n,n,n,n,"/",n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,
+n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,"\\",n,n,n,n,n,"\b",n,n,n,"\f",n,n,n,n,n,n,n,
+`
+`,n,n,n,"\r",n,"	"],g="",S=[],e=65536,X=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,0,4096,8192,
 12288,16384,20480,24576,28672,32768,36864,e,e,e,e,e,e,e,40960,45056,49152,53248,
 57344,61440,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,40960,45056,49152,
-53248,57344,61440]),O=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,
+53248,57344,61440]),J=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,0,256,512,768,1024,1280,1536,
 1792,2048,2304,e,e,e,e,e,e,e,2560,2816,3072,3328,3584,3840,e,e,e,e,e,e,e,e,e,e,e,
-e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,2560,2816,3072,3328,3584,3840]),H=new Uint32Array(
+e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,2560,2816,3072,3328,3584,3840]),N=new Uint32Array(
 [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,0,16,32,48,64,80,96,112,128,144,e,e,e,e,e,e,e,160,176,192,208,224,
 240,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,160,176,192,208,224,240]),
-K=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,
+O=new Uint32Array([e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,
 e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,0,1,2,3,4,5,6,7,8,9,e,e,e,e,e,e,e,10,11,12,13,
-14,15,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,10,11,12,13,14,15]);let u,
-a,c,t,s,f,h;function P(o){const i=[],d=[],l=[];u=0,a=32,c=0,t=0,s=U,f=p,h=void 0;
-function r(w){return new v(w+`
-At character `+u+" in JSON: "+o)}e:for(;;){do a=o.charCodeAt(u++);while(a<33&&(a===
-32||a===10||a===13||a===9));switch(a){case 44:switch(c){case 6:s[f]=h,c=3;continue;case 9:
-s[s.length]=h,c=8;continue;default:throw r("Unexpected comma")}case 58:switch(c){case 4:
-c=5;continue;default:throw r("Unexpected colon")}case 34:h="";n:for(;;){C.lastIndex=
-u,C.test(o);const g=C.lastIndex;switch(g>u&&(h+=o.slice(u,g),u=g),a=o.charCodeAt(
-u++),a){case 34:break n;case 92:if(a=o.charCodeAt(u++),a===117){const m=J[o.charCodeAt(
-u++)]+O[o.charCodeAt(u++)]+H[o.charCodeAt(u++)]+K[o.charCodeAt(u++)];if(m<65536){
-h+=String.fromCharCode(m);continue}throw r("Invalid \\uXXXX escape in string")}const A=D[a];
-if(A){h+=A;continue}const E=a>=0?"'\\"+String.fromCharCode(a)+"'":"end of input";
-throw r("Invalid escape sequence "+E+" in string")}if(isNaN(a))throw r("Untermin\
-ated string");const S=a===10?"newline":a===9?"tab":"control character",b=a.toString(
-16),I="0000".slice(b.length)+b;throw r("Invalid unescaped "+S+" (\\u"+I+") in st\
-ring")}switch(c){case 3:case 2:f=h,c=4;continue;case 5:c=6;continue;case 8:case 7:
-c=9;continue;case 0:c=1;continue;default:throw r("Unexpected quote")}case 123:switch(c){case 5:
-l[t]=6,i[t]=s,d[t++]=f,s={},c=2;continue;case 8:case 7:l[t]=9,i[t]=s,d[t++]=p,s=
-{},c=2;continue;case 0:l[t]=1,i[t]=U,d[t++]=p,s={},c=2;continue;default:throw r(
-"Unexpected opening brace")}case 125:switch(c){case 6:s[f]=h;case 2:h=s,s=i[--t],
-f=d[t],c=l[t];continue;default:throw r("Unexpected closing brace")}case 91:switch(c){case 5:
-l[t]=6,i[t]=s,d[t++]=f,s=[],c=7;continue;case 8:case 7:l[t]=9,i[t]=s,d[t++]=p,s=
-[],c=7;continue;case 0:l[t]=1,i[t]=U,d[t++]=p,s=[],c=7;continue;default:throw r(
-"Unexpected opening square bracket")}case 93:switch(c){case 9:s[s.length]=h;case 7:
-h=s,s=i[--t],f=d[t],c=l[t];continue;default:throw r("Unexpected closing square b\
-racket")}default:if(!(a>=0))break e;const w=u-1;if(y.lastIndex=w,!y.test(o))throw r(
-"Unexpected token '"+String.fromCharCode(a)+"' when expecting "+x[c]);switch(u=y.
-lastIndex,a<102?h=+o.slice(w,u):h=a===110?null:a===116,c){case 5:c=6;continue;case 8:case 7:
-c=9;continue;case 0:c=1;continue;default:throw r("Unexpected token '"+h+"' when \
-expecting "+x[c])}}}if(c!==1)throw r("Unexpected end of input when expecting "+x[c]);
-return h}
+14,15,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,10,11,12,13,14,15]);function U(c,r=""){
+if(!(c>=0))return"end of input";if(c>31&&c<127)return`'${r}${String.fromCharCode(
+c)}'`;if(c===10)return"\\n";if(c===9)return"\\t";const u=c.toString(16),p="0000".
+slice(u.length)+u;return(c>31?`'${r}${String.fromCharCode(c)}', `:"")+`\\u${p}`}
+function D(c){const r=[],u=[],p=[];let i=0,s,t=0,a=0,o,l,d;function h(w){return new v(
+`${w}
+At character ${i} in JSON: ${c}`)}e:for(;;){do s=c.charCodeAt(i++);while(s<33&&(s===
+32||s===10||s===13||s===9));switch(s){case 44:switch(t){case 6:o[l]=d,t=3;continue;case 9:
+o[o.length]=d,t=8;continue;default:throw h(`Unexpected ',', expecting ${f[t]}`)}case 58:
+switch(t){case 4:t=5;continue;default:throw h(`Unexpected ':', expecting ${f[t]}`)}case 34:
+d="";n:for(;;){C.lastIndex=i,C.test(c);const x=C.lastIndex;switch(x>i&&(d+=c.slice(
+i,x),i=x),s=c.charCodeAt(i++),s){case 34:break n;case 92:if(s=c.charCodeAt(i++),
+s===117){const A=X[c.charCodeAt(i++)]+J[c.charCodeAt(i++)]+N[c.charCodeAt(i++)]+
+O[c.charCodeAt(i++)];if(A<65536){d+=String.fromCharCode(A);continue}throw h("Inv\
+alid \\uXXXX escape in string")}const k=R[s];if(k){d+=k;continue}throw h(`Invali\
+d escape sequence in string: ${U(s,"\\")}`)}throw s>=0?h(`Invalid unescaped ${U(
+s)} in string`):h("Unterminated string")}switch(t){case 3:case 2:l=d,t=4;continue;case 5:
+t=6;continue;case 8:case 7:t=9;continue;case 0:t=1;continue;default:throw h(`Une\
+xpected '"', expecting ${f[t]}`)}case 123:switch(t){case 5:p[a]=6,r[a]=o,u[a++]=
+l,o={},t=2;continue;case 8:case 7:p[a]=9,r[a]=o,u[a++]=g,o={},t=2;continue;case 0:
+p[a]=1,r[a]=S,u[a++]=g,o={},t=2;continue;default:throw h(`Unexpected '{', expect\
+ing ${f[t]}`)}case 125:switch(t){case 6:o[l]=d;case 2:d=o,o=r[--a],l=u[a],t=p[a];
+continue;default:throw h(`Unexpected '}', expecting ${f[t]}`)}case 91:switch(t){case 5:
+p[a]=6,r[a]=o,u[a++]=l,o=[],t=7;continue;case 8:case 7:p[a]=9,r[a]=o,u[a++]=g,o=
+[],t=7;continue;case 0:p[a]=1,r[a]=S,u[a++]=g,o=[],t=7;continue;default:throw h(
+`Unexpected '[', expecting ${f[t]}`)}case 93:switch(t){case 9:o[o.length]=d;case 7:
+d=o,o=r[--a],l=u[a],t=p[a];continue;default:throw h(`Unexpected ']', expecting ${f[t]}`)}default:
+const w=i-1;if(y.lastIndex=w,!y.test(c)){if(!(s>=0))break e;throw h(`Unexpected ${U(
+s)}, expecting ${f[t]}`)}switch(i=y.lastIndex,s<102?d=+c.slice(w,i):d=s===110?null:
+s===116,t){case 5:t=6;continue;case 8:case 7:t=9;continue;case 0:t=1;continue;default:
+throw h(`Unexpected '${d}', expecting ${f[t]}`)}}}if(t!==1)throw h(`Unexpected e\
+nd of input, expecting ${f[t]}`);return d}
